901a6aa74477e847d1e01bb5091daddb7c4865a7
panaki-gillot <gillotpanaki@gmail.com>
2026-05-12 15:56:44 +0200

    Feat: add Square class definition with initialization method

---
+++ python_oop/classes_and_object_model/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module that defines a Square class."""
+
+
+class Square:
+    """Defines a square with a private size attribute."""
+
+    def __init__(self, size):
+        """Initialize the square with a size."""
+        self.__size = size
